6656f26b952722d46e9b38a551abe39de95211e0
THUMMALAPALLI NIKHIL CHOWDARY <tnikhilchowdary@gmail.com>
2026-01-06 13:05:27 -0600

    Add gitignore and push backend code

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from "express";
+import dotenv from "dotenv";
+
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT;
 
 app.get("/", (req, res) => {
     res.send("Welcome to the Expenses Tracker");
