1d15be633cacfba27f4a933c15da6509c4ccac35
THUMMALAPALLI NIKHIL CHOWDARY <tnikhilchowdary@gmail.com>
2026-01-06 22:50:18 -0600

    Frontend Setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-black flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-green-400">
+        Tailwind is WORKING ✅
+      </h1>
+    </div>
+  );
+}
+
+export default App;
+
+
