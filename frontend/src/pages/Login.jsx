bb83af70b76b4a5ec60e76ae6801f31511618f61
THUMMALAPALLI NIKHIL CHOWDARY <tnikhilchowdary@gmail.com>
2026-01-08 18:29:29 -0600

    Form Setup

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,25 @@
+import {useState} from "react";
 
 const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
     return(
         <div>
             <h1>Welcome to the Login page</h1>
+            <form>
+                <input type="email"
+                placeholder="Enter Email"
+                value={email}
+                onChange={(e) => {setEmail(e.target.value)}}
+                />
+                <input type="password"
+                placeholder="Enter Password"
+                value={password}
+                onChange={(e) => {setPassword(e.target.value)}}
+                />
+                <button>Submit</button>
+            </form>
         </div>
     )
 }
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,8 +1,30 @@
-
+import {useState} from "react";
 const Signup = () => {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
     return(
         <div>
             <h1>Welcome to the Signup page</h1>
+            <form>
+                <input type="text" 
+                placeholder="Enter Name" 
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                />
+                <input type="email"
+                placeholder="Enter Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                />  
+                <input type="password"
+                placeholder="Enter Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                />
+                <button type="submit">Submit</button>
+            </form>
         </div>
     )
 }
