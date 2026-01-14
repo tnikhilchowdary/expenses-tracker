c6d861f97841f09251918a9a3bf2c0f8a24b4ca9
THUMMALAPALLI NIKHIL CHOWDARY <tnikhilchowdary@gmail.com>
2026-01-14 17:04:34 -0600

    authentication middleware implemented

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,26 @@
+import jwt from "jsonwebtoken";
+
+const authMiddleware = (req, res, next) => {
+    try{
+        const authHeader = req.headers.authorization;
+
+        if(!authHeader){
+            return res.status(401).json({
+                message:"No Token Provided"
+            });
+        }
+
+        const token = authHeader.split(" ")[1];
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.userId = decoded.userId;
+        next();
+    }
+    catch(error){
+        return res.status(401).json({message:"Invalid or Exprired Token"});
+    }
+};
+
+
+export default authMiddleware;
+
